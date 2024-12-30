b5b3b7062965205ac0f22c6681f92883a7592b62
Szymon <91761037+MisterChamster@users.noreply.github.com>
2024-12-30 21:56:36 +0100

    Continuation
    Added a Sound parent class extending Thread and made SoundNote and SoundChord extend it. Also, now SoundChord can have notes added and removed from it via functions. Whole thing is tested.

--- progCurrent.java
+++ progCurrent.java
@@ -1,7 +1,210 @@
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.io.*;
-//chord class?
+//incorporate sample size. PRIORITY
+
+
+abstract class Sound extends Thread{
+    String soundName;
+    float sampleRate;
+    double durationInSeconds;
+    byte[] sampleArray;
+    AudioFormat format;
+    SourceDataLine line;
+
+    Sound(){
+        this.soundName = "";
+        this.sampleRate = 0;
+        this.durationInSeconds = 0;
+        this.sampleArray = null;
+        this.line = null;
+        this.format = null;
+    }
+
+    void setSampleRate(float input){
+        this.sampleRate = input;
+    }
+
+    void setDurationInSec(double input){
+        this.durationInSeconds = input;
+    }
+
+    void setNamee(String input){
+        this.soundName = input;
+    }
+
+    byte[] getSampleArray(){
+        return this.sampleArray;
+    }
+
+    void prepareToPlay(){}
+    void playSound(){}
+    void stopSound(){}
+}
+
+class SoundNote extends Sound{
+    String soundName;
+    float sampleRate;
+    double durationInSeconds;
+    double frequency;
+    byte[] sampleArray;
+    AudioFormat format;
+    SourceDataLine line;
+
+    SoundNote(double durationInSec, double frequency){
+        this.soundName = "";
+        this.sampleRate = 44100;
+        this.durationInSeconds = durationInSec;
+        this.frequency = frequency;
+        this.sampleArray = null;
+        this.line = null;
+        this.format = null;
+    }
+
+    @Override
+    void setSampleRate(float input){
+        this.sampleRate = input;
+    }
+
+    @Override
+    void setDurationInSec(double input){
+        this.durationInSeconds = input;
+    }
+
+    void setFrequency(double input){
+        this.frequency = input;
+    }
+
+    @Override
+    void setNamee(String input){
+        this.soundName = input;
+    }
+
+    @Override
+    byte[] getSampleArray(){
+        return this.sampleArray;
+    }
+
+    void computeSampleArray(){
+        double twoPIFreq = 2.0 * Math.PI * frequency;
+        sampleArray = new byte[(int) Math.ceil(sampleRate * durationInSeconds)];
+
+        for (int i = 0; i < sampleArray.length; i++) {
+            double angle = twoPIFreq * i / sampleRate;
+            sampleArray[i] = (byte) (Math.sin(angle) * 127); //scale to byte range
+        }
+    }
+
+    @Override
+    void prepareToPlay(){
+        computeSampleArray();
+        format = new AudioFormat(sampleRate, 8, 1, true, true); //8 bits sample size
+        try {line = AudioSystem.getSourceDataLine(format);}
+        catch (LineUnavailableException e) {System.exit(0);}
+    }
+
+    @Override
+    void playSound(){
+        try {line.open(format);}
+        catch (LineUnavailableException e) {System.exit(0);}
+        line.start();
+        //here it starts playing
+        line.write(sampleArray, 0, sampleArray.length);
+
+        //cleanup
+        line.drain();
+        line.close();
+    }
+
+    @Override
+    void stopSound(){
+        line.drain();
+        line.close();
+    }
+
+    public void run(){
+        this.playSound();
+    }
+}
+
+class SoundChord extends Sound{
+    String soundName;
+    SoundNote[] noteArray;
+    int noteArrayCLen;
+    float sampleRate;
+    double durationInSeconds;
+    byte[] sampleArray;
+    AudioFormat format;
+    SourceDataLine line;
+
+    SoundChord(){
+        this.soundName = "";
+        this.noteArray = new SoundNote[20];  //max note size of a chord chosen arbitrarily
+        this.noteArrayCLen = 0;
+        this.sampleRate = 0;
+        this.durationInSeconds = 0;
+        this.sampleArray = null;
+        this.line = null;
+        this.format = null;
+    }
+
+    @Override
+    void setSampleRate(float input){
+        this.sampleRate = input;
+    }
+
+    @Override
+    void setDurationInSec(double input){
+        this.durationInSeconds = input;
+    }
+
+    @Override
+    void setNamee(String input){
+        this.soundName = input;
+    }
+
+    @Override
+    byte[] getSampleArray(){
+        return this.sampleArray;
+    }
+
+    void addNote(SoundNote note){
+        if (noteArrayCLen >= noteArray.length - 1){
+            //Resizes the array if the user is stupid enough to use more than 20 notes in a chord
+            SoundNote[] resizeArray = new SoundNote[noteArray.length + 5];
+            for (int i=0; i<noteArray.length; i++){
+                resizeArray[i] = noteArray[i];
+            }
+            noteArray = resizeArray;
+        }
+        noteArray[noteArrayCLen] = note;
+        noteArrayCLen++;
+    }
+
+    void delNote(int index){
+        noteArrayCLen--;
+        for (int i=index+1; i<=noteArrayCLen; i++){
+            noteArray[i-1] = noteArray[i];
+        }
+        noteArray[noteArrayCLen] = null;
+    }
+
+    //debug function
+    void printNoteArray(){
+        for(int i=0; i<noteArrayCLen; i++){
+            System.out.println((i+1) + ". " + noteArray[i].frequency);
+        }
+    }
+
+    void computeSampleArray(){
+
+    }
+
+}
+
+class SoundMix extends Sound{
+
+}
 
 class WavFileSaver {
     //After using this stupid function for some reason You have to use
@@ -79,121 +282,33 @@ class WavFileSaver {
     }
 }
 
-class SoundNote extends Thread{
-    String noteName;
-    float sampleRate;
-    double durationInSeconds;
-    double frequency;
-    byte[] sampleArray;
-    AudioFormat format;
-    SourceDataLine line;
-
-    SoundNote(double durationInSec, double frequency){
-        this.noteName = "";
-        this.sampleRate = 44100;
-        this.durationInSeconds = durationInSec;
-        this.frequency = frequency;
-        this.sampleArray = null;
-        this.line = null;
-        this.format = null;
-    }
-
-    void setSampleRate(float input){
-        this.sampleRate = input;
-    }
-
-    void setDurationInSec(double input){
-        this.durationInSeconds = input;
-    }
-
-    void setFrequency(double input){
-        this.frequency = input;
-    }
-
-    void setNamee(String input){
-        this.noteName = input;
-    }
-
-    byte[] getSampleArray(){
-        return this.sampleArray;
-    }
-
-    void computeSampleArray(){
-        double twoPIFreq = 2.0 * Math.PI * frequency;
-        sampleArray = new byte[(int) Math.ceil(sampleRate * durationInSeconds)];
-
-        for (int i = 0; i < sampleArray.length; i++) {
-            double angle = twoPIFreq * i / sampleRate;
-            sampleArray[i] = (byte) (Math.sin(angle) * 127); //scale to byte range
-        }
-    }
-
-    void prepareToPlay(){
-        computeSampleArray();
-        format = new AudioFormat(sampleRate, 8, 1, true, true); //8 bits sample size
-        try {line = AudioSystem.getSourceDataLine(format);}
-        catch (LineUnavailableException e) {System.exit(0);}
-    }
-
-    void playNote(){
-        try {line.open(format);}
-        catch (LineUnavailableException e) {System.exit(0);}
-        line.start();
-        //here it starts playing
-        line.write(sampleArray, 0, sampleArray.length);
-
-        //cleanup
-        line.drain();
-        line.close();
-    }
-
-    void stopNote(){
-        line.drain();
-        line.close();
-    }
-
-    public void run(){
-        this.playNote();
-    }
-}
-
-class SoundChord extends Thread{
-    String chordName;
-    SoundNote[] noteArray;
-    float sampleRate;
-    double durationInSeconds;
-    double frequency;
-    byte[] sampleArray;
-    AudioFormat format;
-    SourceDataLine line;
-
-    SoundChord(){
-        this.chordName = "";
-        this.noteArray = null;
-        this.sampleRate = 0;
-        this.durationInSeconds = 0;
-        this.frequency = 0;
-        this.sampleArray = null;
-        this.line = null;
-        this.format = null;
-    }
-}
-
-
 public class progCurrent{
     public static void main(String[] args){
-        SoundNote wav1 = new SoundNote(0.5, 440);
-        SoundNote wav2 = new SoundNote(0.5, 659.25);
-        SoundNote wav3 = new SoundNote(0.5, 880);
-        wav1.prepareToPlay();
-        wav2.prepareToPlay();
-        wav3.prepareToPlay();
+        SoundNote note1 = new SoundNote(0.5, 440);
+        SoundNote note2 = new SoundNote(0.5, 659.25);
+        SoundNote note3 = new SoundNote(0.5, 880);
+        note1.prepareToPlay();
+        note2.prepareToPlay();
+        note3.prepareToPlay();
 
-        WavFileSaver.saveWavFile(wav1.sampleArray, 44100, 8);
+        SoundChord chord1 = new SoundChord();
+        for(int i=0; i<13; i++){
+            chord1.addNote(note3);
+        }
+        chord1.addNote(note2);
+        for(int i=0; i<13; i++){
+            chord1.addNote(note3);
+        }
+        chord1.printNoteArray();
+        System.out.println("HDSAIKFDSAKFSJNAKF");
+        chord1.delNote(13);
+        chord1.printNoteArray();
 
-        wav1.prepareToPlay();
-        wav1.start();
-        wav2.start();
-        wav3.start();
+        // WavFileSaver.saveWavFile(note1.sampleArray, 44100, 8);
+
+        // note1.prepareToPlay();
+        // note1.start();
+        // note2.start();
+        // note3.start();
     }
 }
